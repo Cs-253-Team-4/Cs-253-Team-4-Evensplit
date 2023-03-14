9621941f109e8203a1ececbea08964d081232709
gupta-antriksh <124436909+gupta-antriksh@users.noreply.github.com>
2023-03-15 00:28:49 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -16,7 +16,7 @@ const splitwise = require('./Functions/splitwise')
 app.use(cors())
 app.use(express.json())
 
-const url = 'mongodb+srv://antriksh:VTg1Sk8H@cluster0.bncmyew.mongodb.net/testing?retryWrites=true&w=majority';
+const url = 'mongodb://localhost:27017/Cs-253';
 
 mongoose.connect(url, {
     useNewUrlParser: true,
