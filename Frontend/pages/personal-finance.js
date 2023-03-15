026522c62273cc3ba4150f3d0a3e0c4b2ac3c018
gupta-antriksh <124436909+gupta-antriksh@users.noreply.github.com>
2023-03-16 01:35:49 +0530

    history api integrated

--- Frontend/pages/personal-finance.js
+++ Frontend/pages/personal-finance.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 import { Header } from "components/Header";
 import { Balance } from "components/Balance";
 import { IncomeExpenses } from "components/IncomeExpenses";
 import { TransactionList } from "components/TransactionList";
+import { Transaction } from "@/components/Transaction";
 import { AddTransaction } from "components/AddTransaction";
 import Navbar from "../components/Navbar";
 
@@ -10,29 +12,58 @@ import { GlobalProvider } from "context/GlobalState";
 
 //import 'pages/App.css';
 
-function App() {
-  return (
-    <GlobalProvider>
-      <Navbar></Navbar>
-      <Header />
-      <main className="flex flex-col flex-1 text-center m-10">
-        <div className="flex">
-          <div className="w-1/3 flex p-7 m-5 rounded-2xl shadow-2xl">
-            <div>
-              <Balance />
-              <IncomeExpenses />
-            </div>
-          </div>
-          <div className="w-1/3 flex flex-col p-7 m-5 rounded-2xl shadow-2xl">
-            <AddTransaction />
-          </div>
-          <div className="w-1/3 flex pu-7 m-5 justify-center rounded-2xl shadow-2xl">
-            <TransactionList />
-          </div>
-        </div>
-      </main>
-    </GlobalProvider>
-  );
-}
+const App = () => {
+  const [users, setUsers] = useState([])    
+  const fetchUserData = () => {
+    fetch('http://localhost:1337/api/getPersonalExpenseHistory', {
+      method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token': localStorage.getItem('token'),
+                  }
+            }).then(res => {
+              return res.json()
+            }).then(data => {
+              setUsers(data.personalExpenseHistory)
+            })
+          }
+          useEffect(() => {
+            fetchUserData()
+          }, [])
 
+              return (
+                <GlobalProvider>
+                  <Navbar></Navbar>
+                  <Header />
+                  <main className="flex flex-col flex-1 text-center m-10">
+                    <div className="flex">
+                      <div className="w-1/3 flex p-7 m-5 rounded-2xl shadow-2xl">
+                        <div>
+                          <Balance />
+                          <IncomeExpenses />
+                        </div>
+                      </div>
+                      <div className="w-1/3 flex flex-col p-7 m-5 rounded-2xl shadow-2xl">
+                        <AddTransaction />
+                      </div>
+                      <div className="w-1/3 flex pu-7 m-5 justify-center rounded-2xl shadow-2xl">
+                      <div className='justify-end m-2 p-3'>
+                        <h3 className='text-2xl font-bold text-gray-500 m-2'>History</h3>
+                        <ul className="list">
+                          {/* {tempdata.map(transaction => (<Transaction key={transaction.Title} transaction={transaction} />))} */}
+                          {users.map(user => (
+                            <li key={user.Time}>{user.Title} {user.Amount}</li>
+                          ))}
+                        </ul>
+                      </div>
+                      </div>
+                    </div>
+                  </main>
+                </GlobalProvider>
+              );
+            };       
+          
+        
+
+        
 export default App;
