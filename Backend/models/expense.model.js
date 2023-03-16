c4d0b5d06fe1531ecf5bc870cd116b76dc32b9c1
gupta-antriksh <124436909+gupta-antriksh@users.noreply.github.com>
2023-03-16 16:30:18 +0530

    add friend requests array

--- Backend/models/expense.model.js
+++ Backend/models/expense.model.js
@@ -4,8 +4,9 @@ const Expense = new mongoose.Schema(
 	{
 		email : {type: String, required: true, unique: true},
 		personal : {type: Array, "default":[]},
-        friends : {type: Array, "default":[]},
-        groups : {type: Array, "default":[]},	//array stores group ids
+		friends : {type: Array, "default":[]},
+		requests : {type: Array, "default":[]},
+		groups : {type: Array, "default":[]},	//array stores group ids
 	},
 	{ collection: 'expense-data' }
 )
