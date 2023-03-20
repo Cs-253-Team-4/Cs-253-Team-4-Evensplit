bb6142bc3d611774c8564eb80f91a03fb2b8bdd0
Aditya Ajmera <98918167+ajmeraditya@users.noreply.github.com>
2023-03-20 23:08:59 +0530

    Update NavItem.js

--- Frontend/components/NavItem.js
+++ Frontend/components/NavItem.js
@@ -1,21 +1,25 @@
 import Link from "next/link";
 const NavItem = ({ text, href, active }) => {
-  if (text === "Sign-Out") {
-    if (typeof window !== "undefined") {
-      localStorage.setItem("token", "");
-    }
-    return (
-      <Link href={href} className={`nav__link`}>
-        {text}
-      </Link>
-    );
-  } else {
-    return (
-      <Link href={href} className={`nav__link`}>
-        {text}
-      </Link>
-    );
-  }
+  // if (text === "Sign-Out") {
+  //   // if (typeof window !== "undefined") {
+  //   //   localStorage.setItem("token", "");
+  //   // }
+  //   return (
+  //     <Link
+  //       href={href}
+  //       className={`nav__link`}
+  //       // onClick={ localStorage.setItem("token", "")}
+  //     >
+  //       {text}
+  //     </Link>
+  //   );
+  // } else {
+  return (
+    <Link href={href} className={`nav__link`}>
+      {text}
+    </Link>
+  );
+  // }
 };
 
 export default NavItem;
