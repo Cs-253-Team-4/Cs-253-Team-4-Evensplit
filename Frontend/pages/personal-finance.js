188328af976c3dfc7ade99064a6c9837ca9e84a2
Aditya Ajmera <98918167+ajmeraditya@users.noreply.github.com>
2023-04-02 23:29:51 +0530

    Update personal-finance.js
    negative sign in history correction

--- Frontend/pages/personal-finance.js
+++ Frontend/pages/personal-finance.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { Header } from "components/Header";
 import { Balance } from "components/Balance";
 import { IncomeExpenses } from "components/IncomeExpenses";
@@ -7,99 +7,128 @@ import { Transaction } from "@/components/Transaction";
 import { AddTransaction } from "components/AddTransaction";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
-import { GlobalContext } from '../context/GlobalState';
+import { GlobalContext } from "../context/GlobalState";
 
 import { GlobalProvider } from "context/GlobalState";
-
 
 // import 'pages/App.css';
 
 function moneyFormatter(num) {
-  let p = Number(num).toFixed(2).split('.');
+  let p = Number(num).toFixed(2).split(".");
   return (
-    '₹ ' + (p[0].split('')[0]=== '-' ? '-' : '') +
+    "₹ " +
+    (p[0].split("")[0] === "-" ? "-" : "") +
     p[0]
-      .split('')
+      .split("")
       .reverse()
       .reduce(function (acc, num, i, orig) {
-        return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
-      }, '')
+        return num === "-" ? acc : num + (i && !(i % 3) ? "," : "") + acc;
+      }, "")
   );
 }
 
 const App = () => {
-  const [users, setUsers] = useState([])    
-  const [total, setTotal] = useState([])    
-  const [expense, setExpense] = useState([])    
-  const [income, setIncome] = useState([])    
+  const [users, setUsers] = useState([]);
+  const [total, setTotal] = useState([]);
+  const [expense, setExpense] = useState([]);
+  const [income, setIncome] = useState([]);
   const fetchUserData = () => {
-    fetch('http://localhost:1337/api/getPersonalExpenseHistory', {
-      method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'x-access-token': localStorage.getItem('token'),
-                  }
-            }).then(res => {
-              return res.json()
-            }).then(data => {
-              if(data.status == 'error') window.location.href = "/";
-              setUsers(data.personalExpenseHistory.reverse())
-              const amounts = data.personalExpenseHistory.map(transaction => Number(transaction.Amount));
-              setTotal(amounts.reduce((acc, item) => (acc += item), 0));
-              setIncome(amounts.filter(item => item > 0).reduce((acc, item) => (acc += item), 0));
-              setExpense(amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0)*-1);
-            })
-          }          
+    fetch("http://localhost:1337/api/getPersonalExpenseHistory", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "x-access-token": localStorage.getItem("token"),
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        if (data.status == "error") window.location.href = "/";
+        setUsers(data.personalExpenseHistory.reverse());
+        const amounts = data.personalExpenseHistory.map((transaction) =>
+          Number(transaction.Amount)
+        );
+        setTotal(amounts.reduce((acc, item) => (acc += item), 0));
+        setIncome(
+          amounts
+            .filter((item) => item > 0)
+            .reduce((acc, item) => (acc += item), 0)
+        );
+        setExpense(
+          amounts
+            .filter((item) => item < 0)
+            .reduce((acc, item) => (acc += item), 0) * -1
+        );
+      });
+  };
 
-          useEffect(() => {
-            if(window.localStorage.getItem('token') == null || window.localStorage.getItem('user') == null) window.location.href="/"
-            fetchUserData()
-          }, [])
+  useEffect(() => {
+    if (
+      window.localStorage.getItem("token") == null ||
+      window.localStorage.getItem("user") == null
+    )
+      window.location.href = "/";
+    fetchUserData();
+  }, []);
 
-              return (
-                
-                <GlobalProvider>
-                  <Navbar></Navbar>
-                  <Header />
-                  <main className="flex flex-col flex-1 text-center m-10">
-                    <div className="flex">
-                      <div className="w-1/3 flex p-7 m-5 rounded-2xl shadow-2xl">
-                        <div>
-                          {/* <Balance /> */}
-                          {/* <IncomeExpenses /> */}
-                          <div className='flex justify-center text-xl font-semibold text-gray-500'>
-                            <h1>Your Balance {moneyFormatter(total)}</h1>
-                          </div>
-                          <div className="inc-exp-container justify-center flex align-center">
-                            <div>
-                              <p className="money plus p-3">Income <br></br>{moneyFormatter(income)}</p>
-                            </div>
-                            <div>
-                              <p className="money minus p-3">Expense <br></br>{moneyFormatter(expense)}</p>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="w-1/3 flex flex-col p-7 m-5 rounded-2xl shadow-2xl">
-                        <AddTransaction />
-                      </div>
-                      <div className="w-1/3 flex pu-7 m-5 justify-center rounded-2xl shadow-2xl">
-                      <div className='justify-end m-2 p-3'>
-                        <h3 className='text-2xl font-bold text-gray-500 m-2'>History</h3>
-                        <ul className="list" style={{textAlign:"left", width:"300px"}}>
-                          {/* {tempdata.map(transaction => (<Transaction key={transaction.Title} transaction={transaction} />))} */}
-                          {users.map(user => (
-                                <li className={user.Amount < 0 ? 'minus' : 'plus'} key={user.Time}>₹  {user.Amount} <br/>{user.Title}</li>
-                              
-                          ))}
-                        </ul>
-                      </div>
-                      </div>
-                    </div>
-                  </main>
-                  <Footer/>
-                </GlobalProvider>
-              );
-            };   
-          
+  return (
+    <GlobalProvider>
+      <Navbar></Navbar>
+      <Header />
+      <main className="flex flex-col flex-1 text-center m-10">
+        <div className="flex">
+          <div className="w-1/3 flex p-7 m-5 rounded-2xl shadow-2xl">
+            <div>
+              {/* <Balance /> */}
+              {/* <IncomeExpenses /> */}
+              <div className="flex justify-center text-xl font-semibold text-gray-500">
+                <h1>Your Balance {moneyFormatter(total)}</h1>
+              </div>
+              <div className="inc-exp-container justify-center flex align-center">
+                <div>
+                  <p className="money plus p-3">
+                    Income <br></br>
+                    {moneyFormatter(income)}
+                  </p>
+                </div>
+                <div>
+                  <p className="money minus p-3">
+                    Expense <br></br>
+                    {moneyFormatter(expense)}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/3 flex flex-col p-7 m-5 rounded-2xl shadow-2xl">
+            <AddTransaction />
+          </div>
+          <div className="w-1/3 flex pu-7 m-5 justify-center rounded-2xl shadow-2xl">
+            <div className="justify-end m-2 p-3">
+              <h3 className="text-2xl font-bold text-gray-500 m-2">History</h3>
+              <ul
+                className="list"
+                style={{ textAlign: "left", width: "300px" }}
+              >
+                {/* {tempdata.map(transaction => (<Transaction key={transaction.Title} transaction={transaction} />))} */}
+                {users.map((user) => (
+                  <li
+                    className={user.Amount < 0 ? "minus" : "plus"}
+                    key={user.Time}
+                  >
+                    ₹ {Math.abs(user.Amount)} <br />
+                    {user.Title}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </GlobalProvider>
+  );
+};
+
 export default App;
