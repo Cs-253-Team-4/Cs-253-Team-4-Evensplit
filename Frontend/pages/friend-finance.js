9d9941d9124bbda5029f30a304f28f5691436c9e
Aditya Ajmera <98918167+ajmeraditya@users.noreply.github.com>
2023-04-16 23:08:00 +0530

    css

--- Frontend/pages/friend-finance.js
+++ Frontend/pages/friend-finance.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { Header } from "components/Header";
 import Footer from "../components/Footer";
 import { SearchBar } from "components/SearchBar";
@@ -8,73 +8,86 @@ import { History } from "components/History";
 
 // import { TransactionList } from 'components/TransactionList';
 import { AddRequest } from "../components/AddRequest";
-import { AddFriendTransaction } from "../components/AddFriendTransaction" 
+import { AddFriendTransaction } from "../components/AddFriendTransaction";
 import Navbar from "../components/Navbar";
 
 import { GlobalProvider } from "context/GlobalState";
 import { PendingListDisplay } from "@/components/PendingListDisplay";
 
-//import 'pages/App.css';   
+//import 'pages/App.css';
 
-async function handleRequest(index, accepted, request){
-  fetch('http://localhost:1337/api/deleteRequest', {
-    method: 'POST',
+async function handleRequest(index, accepted, request) {
+  fetch("http://localhost:1337/api/deleteRequest", {
+    method: "POST",
     headers: {
-        'Content-Type': 'application/json',
-        'x-access-token': localStorage.getItem('token'),
-      },
+      "Content-Type": "application/json",
+      "x-access-token": localStorage.getItem("token"),
+    },
     body: JSON.stringify({
-        index: index,
-        accepted: accepted
-    }),  
-  }).then(res => {
-    return res.json()
-  }).then(data => {
-    window.location.reload();
+      index: index,
+      accepted: accepted,
+    }),
   })
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      window.location.reload();
+    });
 }
 
 function App() {
-  const [friendHistory, setFriendHistory] = useState([])    
-  const [PendingList, setPendingList] = useState([])    
-  const [users, setUsers] = useState([])   
-  const [query , setQuery] = useState("");
+  const [friendHistory, setFriendHistory] = useState([]);
+  const [PendingList, setPendingList] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [query, setQuery] = useState("");
   const keys = ["name", "email"];
   const search = (data) => {
-      return data.filter((item) =>
-          keys.some((key) => item[key].toLowerCase().includes(query))
-      );
-  }; 
+    return data.filter((item) =>
+      keys.some((key) => item[key].toLowerCase().includes(query))
+    );
+  };
   const fetchUserData = () => {
-    fetch('http://localhost:1337/api/getFriendsHistory', {
-      method: 'GET',
+    fetch("http://localhost:1337/api/getFriendsHistory", {
+      method: "GET",
       headers: {
-          'Content-Type': 'application/json',
-          'x-access-token': localStorage.getItem('token'),
-        }
-    }).then(res => {
-      return res.json()
-    }).then(data => {
-      setFriendHistory(data.friendsHistory.reverse())
-      setPendingList(data.requests.reverse());
-    }).then(fetch('http://localhost:1337/api/getUsers', {
-      method: 'GET',
-      headers: {
-          'Content-Type': 'application/json',
-          'x-access-token': localStorage.getItem('token'),
-        }
-    }).then(res => {
-      return res.json()
-    }).then(data => {
-      if(data.status == 'error') window.location.href="/"
-      setUsers(data.users)
-    }))
-  }
+        "Content-Type": "application/json",
+        "x-access-token": localStorage.getItem("token"),
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setFriendHistory(data.friendsHistory.reverse());
+        setPendingList(data.requests.reverse());
+      })
+      .then(
+        fetch("http://localhost:1337/api/getUsers", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "x-access-token": localStorage.getItem("token"),
+          },
+        })
+          .then((res) => {
+            return res.json();
+          })
+          .then((data) => {
+            if (data.status == "error") window.location.href = "/";
+            setUsers(data.users);
+          })
+      );
+  };
 
   useEffect(() => {
-    if(window.localStorage.getItem('token') == null || window.localStorage.getItem('user') == null) window.location.href="/"
-    fetchUserData()
-  }, [])
+    if (
+      window.localStorage.getItem("token") == null ||
+      window.localStorage.getItem("user") == null
+    )
+      window.location.href = "/";
+    fetchUserData();
+  }, []);
 
   return (
     <GlobalProvider>
@@ -82,24 +95,37 @@ function App() {
 
       <main className="flex flex-col flex-1 text-center m-5">
         <div className="flex">
-          <div className="w-1/3 flex flex-col p-7 m-5 mx-auto rounded-2xl shadow-2xl h-screen">
+          <div className="w-1/3 flex flex-col bg-gray-700 p-7 m-5 mx-auto rounded-2xl shadow-2xl h-screen">
             {/* <Balance/> */}
             {/* <IncomeExpenses/> */}
             {/* <SearchBar /> */}
             {/* <PendingListDisplay /> */}
             <div>
-            {/* <h3 className='text-2xl font-bold text-gray-300 m-2' style={{borderBottom:"thick solid gray" }}>Search</h3> */}
-              <form >
-                      <input className="search w-80 text-center border-2 border-gray-600 bg-gray-100 m-1 p-1 text-black rounded-full" placeholder="Search..." onChange={(e) => setQuery(e.target.value.toLowerCase())} />
-                  <div className="overflow-auto h-64">
-                      {search(users).map((item) => (
-                        item.email == localStorage.getItem('user') ? null :
-                          <button key={item._id} className="px-5 py-2 m-1 border-r-4 border-b-4 border-t-2 border-l-2 border-blue-400 rounded-lg w-80" onClick={(e) => {e.preventDefault(); navigator.clipboard.writeText(item.email); alert('Email Copied to Clipboard')}}> 
-                              <p> {item.name}</p>
-                              <p> {item.email}</p>
-                          </button>
-                      ))}
-                  </div>
+              {/* <h3 className='text-2xl font-bold text-gray-300 m-2' style={{borderBottom:"thick solid gray" }}>Search</h3> */}
+              <form>
+                <input
+                  className="search w-80 text-center border-2 border-gray-600 bg-gray-100 m-1 p-1 text-black rounded-full"
+                  placeholder="Search..."
+                  onChange={(e) => setQuery(e.target.value.toLowerCase())}
+                />
+                <div className="overflow-auto h-64">
+                  {search(users).map((item) =>
+                    item.email == localStorage.getItem("user") ? null : (
+                      <button
+                        key={item._id}
+                        className="px-5 py-2 m-1 border-r-4 border-b-4 border-t-2 border-l-2 border-blue-400 rounded-lg w-80"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          navigator.clipboard.writeText(item.email);
+                          alert("Email Copied to Clipboard");
+                        }}
+                      >
+                        <p> {item.name}</p>
+                        <p> {item.email}</p>
+                      </button>
+                    )
+                  )}
+                </div>
               </form>
             </div>
             <h3
@@ -113,23 +139,33 @@ function App() {
                 {PendingList.map((request, index) => {
                   return (
                     <div
-                    className="btn btn-custom btn-lg page-scroll"
-                    style={{textAlign: 'left', textTransform: 'lowercase'}}
+                      className="btn btn-custom btn-lg page-scroll"
+                      style={{ textAlign: "left", textTransform: "lowercase" }}
                     >
                       <p className="justify-start w-60 items-center">
                         {" "}
-                        {request.senderName} ({request.senderEmail}) Requested ₹{request.amount}{" "}
-                        Message: {request.message}
+                        {request.senderName} ({request.senderEmail}) Requested ₹
+                        {request.amount} Message: {request.message}
                       </p>
                       <div className="text-right">
                         <form>
-                        <button class="rounded-full w-15 py-0.5 px-3 m-1  bg-green-600 " onClick={() => {handleRequest({index},true,{request})}}>
-                          {" "}
-                          Accept{" "}
-                        </button>
-                        <button class="rounded-full w-15 py-0.5 px-2 m-1 bg-red-600" onClick={() => {handleRequest({index},false,{request})}}>
-                          Decline
-                        </button>
+                          <button
+                            class="rounded-full w-15 py-0.5 px-3 m-1  bg-green-600 "
+                            onClick={() => {
+                              handleRequest({ index }, true, { request });
+                            }}
+                          >
+                            {" "}
+                            Accept{" "}
+                          </button>
+                          <button
+                            class="rounded-full w-15 py-0.5 px-2 m-1 bg-red-600"
+                            onClick={() => {
+                              handleRequest({ index }, false, { request });
+                            }}
+                          >
+                            Decline
+                          </button>
                         </form>
                       </div>
                       {/* <p> {user.Amount}</p>
@@ -140,11 +176,11 @@ function App() {
               </ul>
             </div>
           </div>
-          <div className="w-1/3 flex flex-col p-7 m-5 rounded-2xl shadow-2xl h-screen">
+          <div className="w-1/3 flex flex-col bg-gray-700 p-7 m-5 rounded-2xl shadow-2xl h-screen">
             <AddFriendTransaction />
             <AddRequest />
           </div>
-          <div className="w-1/3 flex p-7 m-5 mx-auto justify-center rounded-2xl shadow-2xl h-screen">
+          <div className="w-1/3 flex p-7 m-5 bg-gray-700 mx-auto justify-center rounded-2xl shadow-2xl h-screen">
             {/* <History /> */}
             <div className="App">
               <h3
@@ -161,8 +197,12 @@ function App() {
                       <div className="px-5 py-2 m-3 border-r-4 border-b-4 border-t-2 border-l-2 border-red-600 rounded-lg bg-color">
                         <p>
                           {" "}
-                          You paid ₹{-user.amount} to {user.friendName} ({user.friendEmail})
-                          <br/>{user.message == ""? null : <p>Message: {user.message}</p>}
+                          You paid ₹{-user.amount} to {user.friendName} (
+                          {user.friendEmail})
+                          <br />
+                          {user.message == "" ? null : (
+                            <p>Message: {user.message}</p>
+                          )}
                         </p>
                         {/* <p> ₹ {user.Amount}</p>
                       <p> {user.Description}</p> */}
@@ -173,8 +213,12 @@ function App() {
                       <div className="px-5 py-2 m-3 border-r-4 border-b-4 border-t-2 border-l-2 border-green-600 rounded-lg ">
                         <p>
                           {" "}
-                          You received ₹{user.amount} from {user.friendName} ({user.friendEmail})
-                          <br/>{user.message == ""? null : <p>Message: {user.message}</p>}
+                          You received ₹{user.amount} from {user.friendName} (
+                          {user.friendEmail})
+                          <br />
+                          {user.message == "" ? null : (
+                            <p>Message: {user.message}</p>
+                          )}
                         </p>
                         {/* <p> ₹ {user.Amount}</p>
                       <p> {user.Description}</p> */}
@@ -194,7 +238,7 @@ function App() {
           </div>
         </div>
       </main>
-      <Footer/>
+      <Footer />
     </GlobalProvider>
   );
 }
