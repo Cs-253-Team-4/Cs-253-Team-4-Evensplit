fcad425e77cf67b0ddfff155ccae5de69a5e6a8d
Aditya Ajmera <98918167+ajmeraditya@users.noreply.github.com>
2023-04-16 23:09:49 +0530

    css

--- Frontend/pages/personal-finance.js
+++ Frontend/pages/personal-finance.js
@@ -79,22 +79,34 @@ const App = () => {
       <Header />
       <main className="flex flex-col flex-1 text-center m-10">
         <div className="flex">
-          <div className="w-1/3 flex p-7 m-5 rounded-2xl shadow-2xl">
+          <div className="w-1/3 bg-gray-700 flex p-7 m-5 rounded-2xl shadow-2xl">
             <div>
               {/* <Balance /> */}
               {/* <IncomeExpenses /> */}
-              <div className="flex justify-center text-xl font-semibold text-gray-300" style={{ borderBottom: "thick solid gray" }}>
+              <div
+                className="flex justify-center text-xl font-semibold text-gray-300"
+                style={{ borderBottom: "thick solid gray" }}
+              >
                 <h1>Your Balance {moneyFormatter(total)}</h1>
               </div>
-              <div className="inc-exp-container justify-center flex align-center" style={{backgroundColor: 'black'}}>
+              <div
+                className="inc-exp-container justify-center flex align-center"
+                style={{ backgroundColor: "black" }}
+              >
                 <div>
-                  <p className="money plus p-3" style={{backgroundColor: 'black'}}>
+                  <p
+                    className="money plus p-3"
+                    style={{ backgroundColor: "black" }}
+                  >
                     Income <br></br>
                     {moneyFormatter(income)}
                   </p>
                 </div>
                 <div>
-                  <p className="money minus p-3"  style={{backgroundColor: 'black'}}>
+                  <p
+                    className="money minus p-3"
+                    style={{ backgroundColor: "black" }}
+                  >
                     Expense <br></br>
                     {moneyFormatter(expense)}
                   </p>
@@ -102,12 +114,17 @@ const App = () => {
               </div>
             </div>
           </div>
-          <div className="w-1/3 flex flex-col p-7 m-5 rounded-2xl shadow-2xl">
+          <div className="w-1/3 bg-gray-700 flex flex-col p-7 m-5 rounded-2xl shadow-2xl">
             <AddTransaction />
           </div>
-          <div className="w-1/3 flex pu-7 m-5 justify-center rounded-2xl shadow-2xl">
+          <div className="w-1/3 flex pu-7 m-5 bg-gray-700 justify-center rounded-2xl shadow-2xl">
             <div className="justify-end m-2 p-3">
-              <h3 className="text-2xl font-bold text-gray-300 m-2" style={{ borderBottom: "thick solid gray" }}>History</h3>
+              <h3
+                className="text-2xl font-bold text-gray-300 m-2"
+                style={{ borderBottom: "thick solid gray" }}
+              >
+                History
+              </h3>
               <ul
                 className="list"
                 style={{ textAlign: "left", width: "300px" }}
@@ -115,8 +132,12 @@ const App = () => {
                 {/* {tempdata.map(transaction => (<Transaction key={transaction.Title} transaction={transaction} />))} */}
                 {users.map((user) => (
                   <li
-                    className={user.Amount < 0 ? "px-5 py-2 m-3 border-r-4 border-b-4 border-t-2 border-l-2 border-red-600 rounded-lg" : "px-5 py-2 m-3 border-r-4 border-b-4 border-t-2 border-l-2 border-green-600 rounded-lg"}
-                    style={{backgroundColor: 'black', color: 'white'}}
+                    className={
+                      user.Amount < 0
+                        ? "px-5 py-2 m-3 border-r-4 border-b-4 border-t-2 border-l-2 border-red-600 rounded-lg"
+                        : "px-5 py-2 m-3 border-r-4 border-b-4 border-t-2 border-l-2 border-green-600 rounded-lg"
+                    }
+                    style={{ backgroundColor: "black", color: "white" }}
                     key={user.Time}
                   >
                     ₹ {Math.abs(user.Amount)} <br />
