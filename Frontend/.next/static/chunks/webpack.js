b9e0f7d1d823f5dc33badd410baf5ab4ba5fae80
gupta-antriksh <antrikshg21@iitk.ac.in>
2023-04-18 12:08:54 +0530

    add border to income-expenses in personal expense page

--- Frontend/.next/static/chunks/webpack.js
+++ Frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "517a8565cf0487b1"; }
+/******/ 		__webpack_require__.h = function() { return "43b9baf40bf8e5f3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- Frontend/pages/personal-finance.js
+++ Frontend/pages/personal-finance.js
@@ -92,12 +92,12 @@ const App = () => {
               </div>
               <div
                 className="inc-exp-container justify-center flex align-center"
-                style={{ backgroundColor: "#222222", borderWidth: '2px', borderColor: '#999999', borderRadius: '20px' }}
+                style={{ backgroundColor: "#111111", borderWidth: '2px', borderColor: '#999999', borderRadius: '20px' }}
               >
                 <div>
                   <p
                     className="money plus p-3"
-                    style={{ backgroundColor: "#222222" }}
+                    style={{ backgroundColor: "#111111" }}
                   >
                     Income <br></br>
                     {moneyFormatter(income)}
@@ -106,7 +106,7 @@ const App = () => {
                 <div>
                   <p
                     className="money minus p-3"
-                    style={{ backgroundColor: "#222222" }}
+                    style={{ backgroundColor: "#111111" }}
                   >
                     Expense <br></br>
                     {moneyFormatter(expense)}
